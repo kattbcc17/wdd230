a761d4eb5cb3ca7607333eeb8e1c6a5a0095fd6a
kattbcc17 <kattbcc17@byui.edu>
2023-06-21 11:00:51 -0500

    weather api

---
+++ weather api/scripts/watherapi.js
@@ -0,0 +1,37 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// API URL
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=rexburg&units=imperial&appid=5446be76d7c7bdd03fa8dc5f3a9eddb6';
+
+// defined asynchronous function to fetch weather data
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); 
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+// defined function to display weather results
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
